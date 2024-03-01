5cc24020e9aabc1b37d3a14362c207f2dd67956c
Voronin Egor <evoroni2004@gmail.com>
2024-03-01 18:43:13 +0300

    improvement(server): add recv messeges

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,14 +1,27 @@
-const ws = require('ws');
+import { WebSocketServer } from 'ws';
+
 var port = 8080;
 var ip = '127.0.0.1';
 
-const wss = new ws.WebSocketServer({
+const wss = new WebSocketServer({
     port: port, 
     host: ip,
 });
 
 console.log('Server is open on: ', ip, port);
 
-wss.on('connection', () => {
+wss.on('connection', (ws) => {
     console.log('client connected');
+    
+    ws.on('message', (mes) => {
+        console.log(mes);
+    });
+
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+    });
+    
+    ws.send('something');
 });
+
+
